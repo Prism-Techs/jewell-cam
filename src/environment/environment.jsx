7f04f9db21640f3db1ae8264e2a7098f8154f245
Twinkle995 <Twinkle@prismtechincalservice.com>
2024-12-18 19:12:42 +0530

    environment changed

--- src/environment/environment.jsx
+++ src/environment/environment.jsx
@@ -2,7 +2,7 @@ export const environment = {
     // production: false,
     production: true,
 
-    baseurl:'http://cadcambeta-api.prismtechs.in',
+    baseurl:'http://camcadbeta-api.prismtechs.in',
     // baseurl:'http://15.2.2.254:8888',
     // baseurl:'http://15.2.2.80:8000',
 
